a1852462f049bcd5905f0038adf51e7ea38c0592
Alireza <aliareya2003@gmail.com>
2025-07-31 20:40:46 +0430

    dddd

--- src/components/DownloadAppSuggestio.jsx
+++ src/components/DownloadAppSuggestio.jsx
@@ -14,19 +14,19 @@ function DownloadAppSuggestio() {
           Competently disinte rmediate backward.
         </p>
         <div className='w-full flex gap-10 mt-6 sm:mt-3'>
-          <div className='w-40 h-14 flex justify-center items-center bg-white rounded-lg shadow-md shadow-gray-300 mt-5 cursor-pointer hover:bg-red-500 transition-all duration-300'>
+          <div className='w-40 sm:w-48 h-14 flex justify-center items-center bg-white rounded-lg shadow-md shadow-gray-300 mt-5 cursor-pointer hover:bg-red-500 transition-all duration-300'>
             <Icon icon="simple-icons:googleplay" width="24" height="24"  style={{color: '#c20f2f'}} />
             <span className='text-base font-semibold text-gray-800 pl-2'>Google Play</span>
           </div>
-          <div className='w-40 h-14 flex justify-center items-center bg-white rounded-lg shadow-md shadow-gray-300 mt-5 cursor-pointer hover:bg-red-500 transition-all duration-300'>
-            <Icon icon="simple-icons:googleplay" width="24" height="24"  style={{color: '#c20f2f'}} />
-            <span className='text-base font-semibold text-gray-800 pl-2'>Google Play</span>
+          <div className='w-40 sm:w-48 h-14 flex justify-center items-center bg-white rounded-lg shadow-md shadow-gray-300 mt-5 cursor-pointer hover:bg-red-500 transition-all duration-300'>
+            <Icon icon="ri:apple-fill" width="25" height="25"  style={{color: '#c20f2f'}} />
+            <span className='text-base font-semibold text-gray-800 pl-2'>App Store</span>
           </div>
         </div>  
       </div>
       <div className='w-1/2 sm:w-full sm:mt-10 flex items-end h-fit relative'>
         <img src={carimg} className='mt-24' alt="" />
-        <div className='absolute  top-0 right-40 md:right-20 w-40 h-20 bg-white shadow-lg rounded-lg '>
+        <div className='absolute  top-0 right-40 md:right-20 sm:right-16 w-40 h-20 bg-white shadow-lg rounded-lg '>
           <Icon className='absolute -bottom-5 right-16 ' icon="bxs:down-arrow" width="30" height="30"  style={{color:' #fff'}} />
           <div className='w-full flex gap-2 justify-start items-center h-full p-2 '>
             <div className='w-12 h-12 bg-red-100 rounded-full flex justify-center items-center '>  
